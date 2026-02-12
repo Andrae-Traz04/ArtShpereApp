1264198322a80ebc1dc29187fd5fd62c1248c33c
Andrae-Traz04 <trazareskeithandrae@gmail.com>
2026-02-12 16:30:25 +0800

    Describe what you changed

--- Artsphere/vite.config.js
+++ Artsphere/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',           // tge root folder where index.html is located
-  build: {
-    outDir: 'dist'     // output folder for build
-  }
+  base: "/ArtShpereApp"
 });
